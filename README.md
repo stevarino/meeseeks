7133e20d1a376507ce3ef9bcda9b59c4479f6796
stevarino <oconnor39@gmail.com>
2018-02-14 20:31:44 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Meeseeks
+
+Grab attention on the desktop. Useful for video conferencing. Should work on Windows and Linux.
+
+[Icon source](https://romannurik.github.io/AndroidAssetStudio/icons-launcher.html#foreground.type=clipart&foreground.clipart=search&foreground.space.trim=1&foreground.space.pad=0.25&foreColor=rgba(96%2C%20125%2C%20139%2C%200)&backColor=rgb(139%2C%20195%2C%2074)&crop=0&backgroundShape=square&effects=none&name=meeseeks)
---
+++ meeseeks/meeseeks.py
@@ -0,0 +1,187 @@
+import os
+
+from PyQt5.QtCore import (QAbstractEventDispatcher, QAbstractNativeEventFilter,
+                          QPoint, QRect, QSize, Qt)
+from PyQt5.QtGui import QBrush, QColor, QIcon, QPainter
+from PyQt5.QtWidgets import (QAction, QApplication, QCheckBox, QDesktopWidget,
+                             QGridLayout, QLabel, QMainWindow, QMenu,
+                             QSizePolicy, QSpacerItem, QStyle, QSystemTrayIcon,
+                             QWidget, qApp)
+from pyqtkeybind import keybinder
+
+
+class WinEventFilter(QAbstractNativeEventFilter):
+    def __init__(self, keybinder):
+        self.keybinder = keybinder
+        super().__init__()
+
+    def nativeEventFilter(self, eventType, message):
+        ret = self.keybinder.handler(eventType, message)
+        return ret, 0
+
+class MainWindow(QMainWindow):
+    """
+            Сheckbox and system tray icons.
+            Will initialize in the constructor.
+    """
+    check_box = None
+    tray_icon = None
+    
+    # Override the class constructor
+    def __init__(self):
+        # Be sure to call the super class method
+        super().__init__()
+
+        self.setVisible(False)
+    
+        self.setMinimumSize(QSize(480, 80))             # Set sizes
+        self.setWindowTitle("System Tray Application")  # Set a title
+        central_widget = QWidget(self)                  # Create a central widget
+        self.setCentralWidget(central_widget)           # Set the central widget
+    
+        grid_layout = QGridLayout(self)         # Create a QGridLayout
+        central_widget.setLayout(grid_layout)   # Set the layout into the central widget
+        grid_layout.addWidget(QLabel("Application, which can minimize to Tray", self), 0, 0)
+    
+        # Add a checkbox, which will depend on the behavior of the program when the window is closed
+        self.check_box = QCheckBox('Minimize to Tray')
+        grid_layout.addWidget(self.check_box, 1, 0)
+        grid_layout.addItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Expanding), 2, 0)
+    
+        # Init QSystemTrayIcon
+        self.tray_icon = QSystemTrayIcon(self)
+        icon = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mdpi.png')
+        self.tray_icon.setIcon(QIcon(icon))
+    
+        '''
+            Define and add steps to work with the system tray icon
+            show - show window
+            hide - hide window
+            exit - exit from application
+        '''
+        show_action = QAction("Show", self)
+        quit_action = QAction("Exit", self)
+        hide_action = QAction("Hide", self)
+        show_action.triggered.connect(self.show)
+        hide_action.triggered.connect(self.hide)
+        quit_action.triggered.connect(qApp.quit)
+        tray_menu = QMenu()
+        tray_menu.addAction(show_action)
+        tray_menu.addAction(hide_action)
+        tray_menu.addAction(quit_action)
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.show()
+
+
+        ui = QMainWindow()
+        #ui.setWindowFlags(Qt.FramelessWindowHint)
+        ui.setWindowModality(Qt.NonModal)
+        ui.move(0, 0)
+        ui.setMinimumSize(QSize(480, 80))
+        ui.show()
+
+    
+    # Override closeEvent, to intercept the window closing event
+    # The window will be closed only if there is no check mark in the check box
+    def closeEvent(self, event):
+        if self.check_box.isChecked():
+            event.ignore()
+            self.hide()
+            self.tray_icon.showMessage(
+                "Tray Program",
+                "Application was minimized to Tray",
+                QSystemTrayIcon.Information,
+                2000
+            )
+
+class MyWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.screen_geom = QDesktopWidget().screenGeometry()
+        self.setGeometry(self.screen_geom)
+        self.begin = QPoint()
+        self.end = QPoint()
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+
+    def paintEvent(self, event):
+        qp = QPainter(self)
+        br = QBrush(QColor(255, 0, 0, 255))
+        qp.setBrush(br)
+        width = self.screen_geom.width()
+        height = self.screen_geom.height()
+        offset = 10
+        qp.drawRect(QRect(0, 0, offset, height))
+        qp.drawRect(QRect(0, 0, width, offset))
+        qp.drawRect(QRect(width-offset, 0, offset, height))
+        qp.drawRect(QRect(0, height-offset, width, offset))
+
+
+        begin = self.begin
+        end = self.end
+
+        if begin.x() > end.x():
+            begin, end = QPoint(end.x(), begin.y()), QPoint(begin.x(), end.y())
+        if begin.y() > end.y():
+            begin, end = QPoint(begin.x(), end.y()), QPoint(end.x(), begin.y())
+
+
+        qp.drawRect(QRect( # top
+            begin - QPoint(offset,offset),
+            QPoint(end.x() + offset, begin.y())
+        ))
+        qp.drawRect(QRect( # left
+            begin - QPoint(offset,offset),
+            QPoint(begin.x(), end.y() + offset)
+        ))
+        qp.drawRect(QRect( # bottom
+            end + QPoint(offset,offset), 
+            QPoint(begin.x() - offset, end.y())
+        ))
+        qp.drawRect(QRect( # right
+            end + QPoint(offset,offset), 
+            QPoint(end.x(), begin.y() - offset)
+        ))
+        
+
+    def mousePressEvent(self, event):
+        self.begin = event.pos()
+        self.end = event.pos()
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        self.end = event.pos()
+        self.update()
+
+    def mouseReleaseEvent(self, event):
+        self.begin = event.pos()
+        self.end = event.pos()
+        self.update()
+        self.hide()
+
+def main():
+    import sys
+
+    app = QApplication(sys.argv)
+    win = MainWindow()
+
+    widget = MyWidget()
+
+
+    def callback():
+        widget.show()
+
+    keybinder.init()
+    keybinder.register_hotkey(win.winId(), "Ctrl+Shift+1", callback)
+
+    # Install a native event filter to receive events from the OS
+    win_event_filter = WinEventFilter(keybinder)
+    event_dispatcher = QAbstractEventDispatcher.instance()
+    event_dispatcher.installNativeEventFilter(win_event_filter)
+
+    keybinder.unregister_hotkey(win.winId(), 0x0, 0x0)
+    sys.exit(app.exec())
+
+    
+if __name__ == "__main__":
+    main()
